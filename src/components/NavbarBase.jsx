5a78d4aaaaa5fedbd9448b4ea4cdc6f6d7f44764
Jesus <jesus.l.ortiz11@gmail.com>
2025-09-09 22:24:08 -0300

    feat: creacion del componente Navbarbase

---
+++ src/components/NavbarBase.jsx
@@ -0,0 +1,26 @@
+import CartButton from "./cartButton"
+import { Link } from "react-router-dom"
+
+const NavbarBase = ({user, cart}) => {
+  return (
+    <>
+    <Link className="text-2xl font-bold 
+    text-emerald-400 items-center space-x-2 flex"
+    to="/">
+        E-Commerce
+    </Link>
+
+    <nav className="flex flex-wrap items-center
+    gap-4">
+      <Link className="text-gray-300
+       hover:text-emerald-400 transition
+       duration-300 ease-in-out" to="/">
+        Home
+      </Link>
+      <CartButton user={user} cart={cart}/>
+    </nav>
+    </>
+  )
+}
+
+export default NavbarBase
