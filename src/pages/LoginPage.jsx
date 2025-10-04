19232216c3b4aac57c99a0a019f57f4c601ada0f
Jesus <jesus.l.ortiz11@gmail.com>
2025-10-03 21:15:23 -0300

    se creo el boton de submit al formulario

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -35,6 +35,11 @@ const LoginPage = () => {
                     fieldValue={password}
                     setField={setPassword}
                     />
+                    <button
+                    className='w-full flex justify-center border rounded-md border-transparent
+                    bg-emerald-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-emerald-700
+                    focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2'
+                    type='submit'>Enviar </button>
                 </form>
             </div>
           </div>
